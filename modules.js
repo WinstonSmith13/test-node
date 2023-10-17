b587f385f033d480aa93b1bb9aa2b79b486a58b1
Medy <krouk.medy@gmail.com>
2023-10-17 16:33:03 +0200

    installing express.

--- modules.js
+++
@@ -1,7 +0,0 @@
-const people = ['mario', 'ryu', 'yoshi'];
-const age = [10, 20, 40, 50];
-
-module.exports = {
-    people,
-    age
-}
---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require("fs");
+const _ = require('lodash');
+
+const hostname = "127.0.0.1";
+const port = 8000;
+
+
+const server = http.createServer((req, res) => {
+
+  const num = _.random(0, 20);
+console.log(num)
+  res.setHeader('Content-Type', 'text/html');
+  let path = './views'
+  switch(req.url){
+    case '/': 
+      path += '/index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += '/about.html';
+      res.statusCode = 200;
+      break;
+    default: 
+      path += '/404.html';
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+if ( err) {
+  console.log(err);
+}else {
+  res.end(data);
+}
+  })
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Le serveur tourne à l'adresse http://${hostname}:${port}/`);
+}); 
